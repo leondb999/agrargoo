2bc5edb0a648875c110f466880a2c2dbe88a8a11
leondb999 <led.1999led@gmail.com>
2022-03-21 10:45:52 +0100

    Delete graph ql commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,34 +7,16 @@ void main() {
   runApp(MyApp());
 }
 
-final HttpLink httpLink =
-    HttpLink("https://api.randevu.technology/getParticipantTypes");
-
-GraphQLClient qlClient =
-    GraphQLClient(link: httpLink, cache: GraphQLCache(store: HiveStore()));
-
-final AuthLink authLink = AuthLink(
-    getToken: () async => "Bearer sk_sand-79116408b11c4d3e8ca691a8d1935ee0");
-
-final Link link = authLink.concat(httpLink);
-
-ValueNotifier<GraphQLClient> client = ValueNotifier(
-  GraphQLClient(link: link, cache: GraphQLCache(store: HiveStore())),
-);
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GraphQLProvider(
-      client: client,
-      child: MaterialApp(
-        title: 'Agrargo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: LandingPage(title: 'Agrargo'),
+    return MaterialApp(
+      title: 'Agrargo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: LandingPage(title: 'Agrargo'),
     );
   }
 }
