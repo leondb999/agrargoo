08b495deead69b8c6f58a7bf4aa0ffaee8b62279
leondb999 <led.1999led@gmail.com>
2022-03-21 10:03:44 +0100

    init dev_leon-branch

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Agrargo0',
+      title: 'Agrargo00',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
