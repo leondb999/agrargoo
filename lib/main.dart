1d26460109e66c23642e4a786a15ba91a963fdcf
leondb999 <led.1999led@gmail.com>
2022-03-22 13:47:31 +0100

    Firebase Authentication riverpods

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,50 @@
-import 'package:agrargo/services/authentication_provider.dart';
-import 'package:agrargo/zwischenspeicher/login_page_zw.dart';
-import 'package:agrargo/UI/login/login_page.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'zwischenspeicher/intro_screen.dart';
-import 'UI/landing_page.dart';
+import 'package:agrargo/services/auth_checker.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:provider/provider.dart';
-import 'firebase_options.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:provider/provider.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() async {
+import 'UI/error_screen.dart';
+import 'UI/loading_screen.dart';
+
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+//  This is a FutureProvider that will be used to check whether the firebase has been initialized or not
+final firebaseinitializerProvider = FutureProvider<FirebaseApp>((ref) async {
+  return await Firebase.initializeApp();
+});
+
+class MyApp extends ConsumerWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<AuthenticationProvider>(
-          create: (_) => AuthenticationProvider(FirebaseAuth.instance),
-        ),
-        StreamProvider(
-          create: (context) => context.read<AuthenticationProvider>().authState,
-          initialData: null,
-        )
-      ],
-      child: MaterialApp(
-        title: 'AgrarGo',
-        home: Authenticate(),
-      ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    //  We will watch this provider to see if the firebase has been initialized
+    //  As said this gives async value so it can gives 3 types of results
+    //  1. The result is a Future<FirebaseApp>
+    //  2. The result is a Future<Error>
+    //  3. It's still loading
+
+    final initialize = ref.watch(firebaseinitializerProvider);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      //  We will use the initialize to check if the firebase has been initialized
+      //  .when function can only be used with AsysncValue. If you hover over intialize
+      //  you can see what type of variable it is. I have left it dynamic here for your better understanding
+      //  Though it's always recommended to not to use dynamic variables.
+
+      // Now here if the Firebase is initialized we will be redirected to AuthChecker
+      // which checks if the user is logged in or not.
+
+      //  the other Two functions speaks for themselves.
+      home: initialize.when(
+          data: (data) {
+            return const AuthChecker();
+          },
+          loading: () => const LoadingScreen(),
+          error: (e, stackTrace) => ErrorScreen(e, stackTrace)),
     );
   }
 }
-
-class Authenticate extends StatelessWidget {
-  const Authenticate({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    final firebaseUser = context.watch<User>();
-    if (firebaseUser != null) {
-      return LandingPage();
-    }
-    return LoginPage();
-  }
-}
-
-/*
-return Provider<FirebaseAuth>(
-      create: (_) => FirebaseAuth.instance,
-      child: MaterialApp(
-        title: 'Agrargo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: LandingPage(),
-      ),
-    );
- */
