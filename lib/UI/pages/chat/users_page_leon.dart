493eaa473a94bfe9f5fce1f4f06b181a38c8023b
christinaderst <christina.derst@gmx.de>
2022-04-12 22:43:27 +0200

    Chat Searchbar Stand 1

--- lib/UI/pages/chat/users_page_leon.dart
+++ lib/UI/pages/chat/users_page_leon.dart
@@ -5,6 +5,7 @@ import 'package:agrargo/controllers/auth_controller.dart';
 import 'package:agrargo/controllers/user_controller.dart';
 import 'package:agrargo/models/user_model.dart';
 import 'package:agrargo/provider/user_provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -17,6 +18,7 @@ import '../../../allConstants/size_constants.dart';
 import '../../../repositories/firestore_user_model_riverpod_repository.dart';
 import '../../../widgets/layout_widgets.dart';
 import 'chatDetailPage.dart';
+import 'package:searchfield/searchfield.dart';
 
 /// UserPage | Quelle: https://github.com/flyerhq/flutter_firebase_chat_core/blob/main/example/lib/users.dart
 class ChatUsersPage extends ConsumerStatefulWidget {
@@ -30,10 +32,12 @@ class ChatUsersPage extends ConsumerStatefulWidget {
 class _ChatUsersPageState extends ConsumerState<ChatUsersPage> {
   TextEditingController searchTextEditingController = TextEditingController();
   StreamController<bool> buttonClearController = StreamController<bool>();
-  String _textSearch = "";
+  TextEditingController searchController = TextEditingController();
+  List<Map> searchResult = [];
+  bool isLoading = false;
 
   Widget _buildAvatar(UserModel userModel) {
-    final color = Colors.green;
+    final color = Color(0xFF9FB98B);
     final hasImage = userModel.profilImageURL != null;
     final name = userModel.name;
 
@@ -66,6 +70,33 @@ class _ChatUsersPageState extends ConsumerState<ChatUsersPage> {
         ),
       ),
     );
+  }
+
+  void onSearch() async {
+    setState(() {
+      searchResult = [];
+      isLoading = true;
+    });
+    await FirebaseFirestore.instance
+        .collection('users')
+        .where("name", isEqualTo: searchController.text)
+        .get()
+        .then((value) {
+      if (value.docs.length < 1) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text("Kein Nutzer gefunden")));
+        setState(() {
+          isLoading = false;
+        });
+        return;
+      }
+      value.docs.forEach((user) {
+        searchResult.add(user.data());
+        setState(() {
+          isLoading = false;
+        });
+      });
+    });
   }
 
   List<types.User> typesUserList = [];
@@ -120,13 +151,17 @@ class _ChatUsersPageState extends ConsumerState<ChatUsersPage> {
     print("userLoggedIn: name: ${userLoggedIn.name}");
 
     return Scaffold(
-      appBar: appBar(context: context, ref: ref, home: false),
-      bottomNavigationBar:
-          navigationBar(index: 1, context: context, ref: ref, home: false),
-      body: StreamBuilder<List<types.User>>(
-        ///TODO knackpunkt
-        stream: FirebaseChatCore.instance
-            .users() /*.where((event) {
+        appBar: appBar(context: context, ref: ref, home: false),
+        bottomNavigationBar:
+            navigationBar(index: 1, context: context, ref: ref, home: false),
+        body: Column(children: [
+          buildSearchBar(),
+          SizedBox(height: MediaQuery.of(context).size.height / 50),
+          Expanded(
+            child: StreamBuilder<List<types.User>>(
+              ///TODO knackpunkt
+              stream: FirebaseChatCore.instance
+                  .users() /*.where((event) {
           bool findUser = false;
           event.forEach((user) {
             if (user.id == 'e3id9w9bPecKea74Qpx4qnPAi2R2') {
@@ -138,58 +173,127 @@ class _ChatUsersPageState extends ConsumerState<ChatUsersPage> {
           });
           return findUser;
         })*/
-        ,
+              ,
 
-        ///FirebaseChatCore.instance.users(),
-        initialData: const [],
-        builder: (context, snapshot) {
-          if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Container(
-              alignment: Alignment.center,
-              margin: const EdgeInsets.only(
-                bottom: 200,
-              ),
-              child: const Text('Kein Nutzer'),
-            );
-          }
+              ///FirebaseChatCore.instance.users(),
+              initialData: const [],
+              builder: (context, snapshot) {
+                if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return Container(
+                    alignment: Alignment.center,
+                    margin: const EdgeInsets.only(
+                      bottom: 200,
+                    ),
+                    child: const Text('Kein Nutzer'),
+                  );
+                }
 
-          return ListView.builder(
-            itemCount: snapshot.data!.length,
-            itemBuilder: (context, index) {
-              final user = snapshot.data![index];
+                return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    final user = snapshot.data![index];
 
-              final userModel =
-                  UserProvider().getUserNameByUserID(user.id, userList).first;
-              //  print("user.id: ${user.id}, userModel.name: ${userModel.name}");
+                    final userModel = UserProvider()
+                        .getUserNameByUserID(user.id, userList)
+                        .first;
+                    //  print("user.id: ${user.id}, userModel.name: ${userModel.name}");
 
-              return FlatButton(
-                onPressed: () {
-                  _handlePressed(user, context);
-                },
-                child: Column(
-                  children: [
-                    Row(children: [
+                    if (searchResult.length > 0) {
                       Expanded(
-                        flex: 1,
-                        child: _buildAvatar(userModel),
+                          child: ListView.builder(
+                              itemCount: searchResult.length,
+                              shrinkWrap: true,
+                              itemBuilder: (context, index) {
+                                return ListTile(
+                                    leading: CircleAvatar(
+                                      child: Image.network(searchResult[index]
+                                          ['profilImageURL']),
+                                    ),
+                                    title: Text(searchResult[index]['name']));
+                              }));
+
+                      return FlatButton(
+                        onPressed: () {
+                          _handlePressed(user, context);
+                        },
+                        child: Column(
+                          children: [
+                            Row(children: [
+                              Expanded(
+                                flex: 1,
+                                child: _buildAvatar(userModel),
+                              ),
+                              Expanded(
+                                flex: 12,
+                                child: Text("${searchResult[index]['name']}"),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child:
+                                    //get room where ${userLoggedIn.UserId) == room.userIDs und userModel.UserId == room.userIDs
+                                    Text("{FirebaseChatCore.instance.room()}"),
+                              ),
+                            ]),
+                          ],
+                        ),
+                      );
+                    }
+                    return FlatButton(
+                      onPressed: () {
+                        _handlePressed(user, context);
+                      },
+                      child: Column(
+                        children: [
+                          Row(children: [
+                            Expanded(
+                              flex: 1,
+                              child: _buildAvatar(userModel),
+                            ),
+                            Expanded(
+                              flex: 12,
+                              child: Text("${userModel.name}"),
+                            ),
+                            Expanded(
+                              flex: 1,
+                              child:
+                                  //get room where ${userLoggedIn.UserId) == room.userIDs und userModel.UserId == room.userIDs
+                                  Text("{FirebaseChatCore.instance.room()}"),
+                            ),
+                          ]),
+                        ],
                       ),
-                      Expanded(
-                        flex: 12,
-                        child: Text("${userModel.name}"),
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child:
-                            //get room where ${userLoggedIn.UserId) == room.userIDs und userModel.UserId == room.userIDs
-                            Text("{FirebaseChatCore.instance.room()}"),
-                      ),
-                    ]),
-                  ],
-                ),
-              );
+                    );
+                  },
+                );
+              },
+            ),
+          )
+        ]));
+  }
+
+  Widget buildSearchBar() {
+    return Padding(
+      padding: EdgeInsets.only(top: 16, left: 16, right: 16),
+      child: TextField(
+        controller: searchController,
+        decoration: InputDecoration(
+          hintText: "Suche nach Nutzern...",
+          hintStyle: TextStyle(color: Colors.grey.shade600),
+          prefixIcon: IconButton(
+            onPressed: () {
+              onSearch();
             },
-          );
-        },
+            icon: Icon(Icons.search),
+            color: Colors.grey.shade600,
+            iconSize: 20,
+          ),
+          filled: true,
+          fillColor: Colors.grey.shade100,
+          contentPadding: EdgeInsets.all(8),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide(color: Colors.grey.shade100)),
+        ),
       ),
     );
   }
